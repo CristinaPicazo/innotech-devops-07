2bea60cf63ff960783cf86c8f726ed3b0f49f32d
Cristina Picazo Merlos <47087596+CristinaPicazo@users.noreply.github.com>
2024-03-16 13:46:42 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,4 +18,16 @@ export default defineConfig({
     },
     setupFiles: ["./test/setup.ts"],
   },
+});
+
+
+export default defineConfig({
+  test: {
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    }
+  }
 });
